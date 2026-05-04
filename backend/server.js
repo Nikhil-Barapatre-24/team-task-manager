17a0d2ec9b9c7e8086ecce8cdf8669a506527b1a
Nikhil Barapatre <nikhil.barapatre@pinnacle.in>
2026-05-04 18:00:17 +0530

    fix: apply gitignore

---
+++ backend/server.js
@@ -0,0 +1,11 @@
+// backend/server.js
+const express = require("express");
+const app = express();
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+    res.send("API Running");
+});
+
+app.listen(5000, () => console.log("Server running"));
